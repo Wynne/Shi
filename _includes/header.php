cd6b9f2bf907c74ff8b13aae1faa766b09e9e0cc
wynne <wynne@monster>
2010-12-15 02:04:42 -0500

    change older name

---
+++ _includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Language" content="en-us" />
+
+	<title> | Home</title>
+
+	<meta name="Keywords" content="__CHANGE__" />
+	<meta name="Description" content="__CHANGE__" />
+		
+	<meta name="ROBOTS" content="ALL" />
+	<meta http-equiv="imagetoolbar" content="no" />
+	<meta name="MSSmartTagsPreventParsing" content="true" />
+	<meta name="Copyright" content="(c) 2010 S.H.I" />
+
+	<link href="_css/stylesheet_sticky.css" rel="stylesheet" type="text/css" media="all" />
+	
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+  <script type="text/javascript" src="_js/script.js"></script>
+	<script type="text/javascript" src="_js/galleria.js"></script>
+	<script type="text/javascript" src="_js/src/plugins/galleria.flickr.js"></script>
+	
+</head>
+<body class="home">
+
+	<!-- Begin Content -->
+<div id="wrapper">
+		<div id="header">
+	      <a class="logo" href="index.php" title="Go to S.H.I homepage"> </a>
+	      <ol id="nav">
+	    	  <li title="Got to the Menu section" <?php echo ($navsection == 'menu') ? ' class="current"' : ''; ?>>
+						<a href="menu.php">Menu</a></li>
+	    	  <li title="Got to the Gallery section" <?php echo ($navsection == 'gallery') ? ' class="current"' : ''; ?>>
+						<a href="gallery.php">Gallery</a></li>
+	    	  <li title="Got to the Events section" <?php echo ($navsection == 'events') ? ' class="current"' : ''; ?>>
+						<a href="events.php">Events</a></li>
+	    	  <li title="Got to the Contact section" <?php echo ($navsection == 'contact') ? ' class="current"' : ''; ?>>
+						<a href="contact.php">Contact</a></li>
+	    	  <li title="Got to the About section" <?php echo ($navsection == 'about') ? ' class="current"' : ''; ?>>
+						<a href="about.php">About</a></li>
+	      </ol>
+		</div><!-- End Header -->
+		<div id="clearheader"></div>
